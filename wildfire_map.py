b5bcd7d1103a2c1239febef9b290b2b08e643372
aahuja07 <108749000+aahuja07@users.noreply.github.com>
2022-08-09 20:30:19 -0400

    New Sattelite Map with Date

--- wildfire_map.py
+++ wildfire_map.py
@@ -27,6 +27,7 @@ for fire in data['events']:
 
 import plotly.graph_objects as go
 import pandas as pd
+import plotly.express as px
 
 df = pd.DataFrame(rows)
 
@@ -35,18 +36,36 @@ split_df = pd.DataFrame(df['coordinates'].tolist(), columns=['longitude', 'latit
 df = pd.concat([df, split_df], axis=1)
 #df
 # https://colab.research.google.com/drive/1oFYdpTah2IYDZqdYOfpWc0poEQW2SKJy?usp=sharing#scrollTo=ZMyUY7XihNta
-fig = go.Figure(data=go.Scattergeo(
-        lon = df['longitude'],
-        lat = df['latitude'],
-        text = df['title'],
-        mode = 'markers',
-        marker=go.scattergeo.Marker(
-            size=6
-        )
-))
+#fig = go.Figure(data=go.Scattergeo(
+#        lon = df['longitude'],
+#        lat = df['latitude'],
+#        text = df['title'],
+#        mode = 'markers',
+#        marker=go.scattergeo.Marker(
+#            size=6
+ #       )
+#))
 
+#fig.update_layout(
+#        title = 'Global Wildfire Tracker',
+#        title_x=0.5
+#    )
+#fig.show()
+
+#sattelite map
+fig = px.scatter_mapbox(df, lat='latitude', lon='longitude', hover_name='title', hover_data= ['date'],
+                       color_discrete_sequence=["red"], zoom=1.5, height=300)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.update_layout(
-        title = 'Global Wildfire Tracker',
-        title_x=0.5
-    )
+    mapbox_style="white-bg",
+    mapbox_layers=[
+     {
+            "below": 'traces',
+            "sourcetype": "raster",
+            "sourceattribution": "United States Geological Survey",
+            "source": [
+                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+        ]}])
 fig.show()
+
